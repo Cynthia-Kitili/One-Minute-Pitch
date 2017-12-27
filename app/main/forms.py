58514d2c3a9bde629cba187a3c3a3cc82a0837b7
Kipngetich33 <khalifngeno@gmail.com>
2017-12-27 02:14:25 -0500

    create the index.html template

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class CommentsForm(FlaskForm):
+    pitch = StringField('Title',validators=[Required()])
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Submit')  
