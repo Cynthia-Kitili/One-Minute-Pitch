f79fd6ecd57a0f5117f0e30f1b971cf37759c56a
Kipngetich33 <khalifngeno@gmail.com>
2017-12-28 10:53:34 -0500

    get email configuartion in the Config.py

--- config.py
+++ config.py
@@ -7,6 +7,13 @@ class Config:
     SECRET_KEY = 'TheDifference'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vincent:Empharse333@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=khalifngeno@gmail.com
+export MAIL_PASSWORD=Empharse333
+python3.6 manage.py server
