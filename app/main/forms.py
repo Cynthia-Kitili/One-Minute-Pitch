2dea332adc265546e7e72b55dd83ce31b9c0b3ee
Kipngetich33 <khalifngeno@gmail.com>
2017-12-28 12:19:57 -0500

    create an update bio route in the main templates folder

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class CommentsForm(FlaskForm):
     pitch = StringField('Title',validators=[Required()])
     comment = TextAreaField('Comment', validators=[Required()])
     submit = SubmitField('Submit')  
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
