d0e52d62e7715fd8eba60a91a622c9aaaeb2430a
Kipngetich33 <khalifngeno@gmail.com>
2017-12-28 03:07:46 -0500

    add the @manage.shell decorater to the __init__.py

--- app/models.py
+++ app/models.py
@@ -34,3 +34,11 @@ class Comment:
                 results.append(comment)
 
         return results
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
