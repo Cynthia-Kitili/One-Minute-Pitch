17ea392e57893a5713a2242d62c9b372ef2b36a0
Kipngetich33 <khalifngeno@gmail.com>
2017-12-28 06:48:08 -0500

    import UserMixin and change the schema of the database

--- app/models.py
+++ app/models.py
@@ -40,7 +40,7 @@ class Comment:
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
+    username = db.Column(db.String(255),index = True) 
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
