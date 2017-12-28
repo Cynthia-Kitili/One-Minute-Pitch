89177742a86cd9432ff4ba4d70b246077bfb989c
Kipngetich33 <khalifngeno@gmail.com>
2017-12-28 05:54:26 -0500

    to the user model addd a new decorators to set the password, verify and block the password from being accessed easily

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db 
+from werkzeug.security import generate_password_hash,check_password_hash
 
 list_of_pitches = [[12,'pitch tweleve'],[14,'pitch 14']]
 class Pitch:
@@ -40,16 +41,30 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
+
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    users = db.relationship('User',backref = 'role',lazy="dynamic") 
 
     def __repr__(self):
-        return f'User {self.name}'
+        return f'User {self.name}'  
