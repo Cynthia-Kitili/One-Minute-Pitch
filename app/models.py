92acf0c0cc945adc4e0b188c14fb28fcb78994bb
Kipngetich33 <khalifngeno@gmail.com>
2017-12-29 07:29:14 -0500

    make changes in order to push to heroku

--- app/models.py
+++ app/models.py
@@ -78,7 +78,7 @@ class User(UserMixin, db.Model):
         self.pass_secure = generate_password_hash(password)
 
     def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+            return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'User {self.username}'
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MAIL_USERNAME=khalifngeno@gmail.com
-export MAIL_PASSWORD=Empharse333
+export MAIL_USERNAME='khalifngeno@gmail.com'
+export MAIL_PASSWORD='Poheri333'
 
 python3.6 manage.py server
