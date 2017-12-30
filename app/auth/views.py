f47a920b82412c12984aa8e2a0d97349d4595479
Kipngetich33 <khalifngeno@gmail.com>
2017-12-30 06:38:29 -0500

    make the final changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,9 @@
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user,logout_user,login_required
 from ..models import User
-from .forms import RegistrationForm, LoginForm 
+from .forms import LoginForm,RegistrationForm
 from .. import db
-from flask_login import login_user, logout_user, login_required 
 from ..email import mail_message
 
 
@@ -21,6 +21,7 @@ def login():
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
     
 
 @auth.route('/register',methods = ["GET","POST"])
