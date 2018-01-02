744cde27baa08988f1f5b162a4e0926b27e2aba9
Kipngetich33 <khalifngeno@gmail.com>
2018-01-02 03:28:56 -0500

    create PitchForm in forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User 
 
