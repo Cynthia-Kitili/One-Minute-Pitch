5fafcdbd939df474ef1a782a37084527c4bbc37c
Kipngetich33 <khalifngeno@gmail.com>
2018-01-03 06:34:58 -0500

    remove duplicate table pitch

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
 class DevConfig(Config):
     '''
     Development  configuration child class
