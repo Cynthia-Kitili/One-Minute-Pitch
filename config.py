a090f95e4d6e16ffcb1fcd44c21cd476f6821b60
Kipngetich33 <khalifngeno@gmail.com>
2018-01-08 05:36:42 -0500

    making changes to database path

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = 'TheDifference'
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vincent:Empharse333@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     #  email configurations
